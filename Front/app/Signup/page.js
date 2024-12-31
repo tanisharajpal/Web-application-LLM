3a34e4e955bd679a72f02c57a3b2c0790e0f3253
tanisharajpal <106838988+tanisharajpal@users.noreply.github.com>
2024-12-31 18:34:58 +0530

    Add files via upload

---
+++ Front/app/Signup/page.js
@@ -0,0 +1,65 @@
+"use client";
+
+import { useEffect, useState } from 'react'
+import { useContext } from "react"
+import { useRouter } from 'next/navigation';
+
+import { AppContext } from "@/controller/context"
+import Spinner from "@/components/Spinner"
+
+export default function SignUp() {
+    const [spinner, setSpinner] = useState(<></>)
+    const {state, AppController} = useContext(AppContext)
+    const router = useRouter();
+
+    useEffect(() => {
+        if(state.token) {
+            router.push('/')
+        }
+      }, [state.token])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        const username = data.get('username');
+        const password = data.get('password');
+        setSpinner(<Spinner />)
+        AppController.sigup(username, password);
+    }
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    {/* <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" /> */}
+                    <img className="w-8 h-8 mr-2" src="logo-renaiss.jpg" alt="logo" />
+                    Renaiss AiChat    
+                </a>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create your new account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+                                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            </div>
+                            <button 
+                                type="submit"
+                                className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            >
+                                Sign up
+                            </button>
+                        </form>
+                        {spinner}
+                    </div>
+                </div>
+            </div>
+            </section>
+    )
+}
